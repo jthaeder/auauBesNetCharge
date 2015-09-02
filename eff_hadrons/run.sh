7b3a65af051f110782d38b7c6bbe9d385d2a1bd7
Jochen Thaeder <jochen@thaeder.de>
2015-09-02 16:23:43 -0700

    add efficiency study

--- eff_hadrons/run.sh
+++ eff_hadrons/run.sh
@@ -2,8 +2,9 @@
 
 ./clean.sh
 
-root -b -l -q fitEfficiencyPt.C++
-root -b -l -q fitEfficiencyEta.C++
+root -l -b -q fitEfficiencyPt.C++
+root -l -b -q fitEfficiencyEta.C++
 root -l -b -q makeEfficiencyCharged.C++
+root -l -b -q makeEfficiencyStudy.C
 
 rm -f *.d *.so
