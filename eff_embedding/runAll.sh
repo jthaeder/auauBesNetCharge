42c5b60bca65501bd7401eb32b3535e7a57d41a9
jthaeder <jochen@thaeder.de>
2015-09-04 12:46:01 -0400

    make dirs

--- eff_embedding/runAll.sh
+++ eff_embedding/runAll.sh
@@ -3,7 +3,7 @@
 energies="7 11 14 19 27 39 62"
 # 200
 
-mkdir -p log
+mkdir -p log embeddingTrees efficiency efficiencyStudy
 
 for energy in $energies ; do 
     for particle in 0 1 2 3 4 5  ; do 
