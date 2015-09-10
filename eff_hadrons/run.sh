b3d20c66e5fde0bd1dc646ec9855a52a863e47f3
Jochen Thaeder <jochen@thaeder.de>
2015-09-09 20:18:53 -0700

    add inset and draw smeared results

--- eff_hadrons/run.sh
+++ eff_hadrons/run.sh
@@ -5,6 +5,6 @@
 root -l -b -q fitEfficiencyPt.C++
 root -l -b -q fitEfficiencyEta.C++
 root -l -b -q makeEfficiencyCharged.C++
-root -l -b -q makeEfficiencyStudy.C++
+#root -l -b -q makeEfficiencyStudy.C++
 
 rm -f *.d *.so
