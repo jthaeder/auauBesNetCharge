7d3d4774aa0b8139e641a42d1ffc147646cf9484
Jochen Thaeder <jochen@thaeder.de>
2016-02-12 18:22:45 +0100

    add missing plotting macros

--- analysis/plots/runPlotingAll.sh
+++ analysis/plots/runPlotingAll.sh
@@ -3,6 +3,11 @@
 ./runPlotingEnergy.sh
 ./runPlotingEta.sh
 
+root -l -b -q makeBESIIerror.C++
+root -l -b -q plotCumulants.C++
+#root -l -b -q plotSetNetProtonCrossCheck.C
+#root -l -b -q plotSetSys.C
+
 root -l -b -q print_STAR_QM2015_Preliminary.C++ > STAR_QM2015_Preliminary.txt
 
 rm -f *.so *.d include/*.so include/*.d
--- analysis/plots/runPlotingEta.sh
+++ analysis/plots/runPlotingEta.sh
@@ -8,6 +8,7 @@ root -l -b -q plotVsEta.C++
 
 root -l -b -q plotEta_nice.C++
 root -l -b -q plotVsDeltaEta_nice.C++
+root -l -b -q plotVsDeltaEta_chargeSep.C
 root -l -b -q plotVsEta_nice.C++
 
 rm -f *.so *.d include/*.so include/*.d
