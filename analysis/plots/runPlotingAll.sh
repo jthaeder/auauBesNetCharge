3c79cb2dd3368e4d83385276840277db2eb0ff70
Jochen Thaeder <jochen@thaeder.de>
2016-02-17 16:24:27 +0100

    creating QM zip file

--- analysis/plots/runPlotingAll.sh
+++ analysis/plots/runPlotingAll.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rm -f STAR_QM2015_Preliminary.root STAR_Preliminary.root
+rm -f *.so *.d include/*.so include/*.d
+
 ./runPlotingEnergy.sh
 ./runPlotingEta.sh
 
@@ -8,6 +11,12 @@ root -l -b -q plotCumulants.C++
 #root -l -b -q plotSetNetProtonCrossCheck.C
 #root -l -b -q plotSetSys.C
 
+
+# -- Create QM 2015 data files
 root -l -b -q print_STAR_QM2015_Preliminary.C++ > STAR_QM2015_Preliminary.txt
+
+rm -f STAR_QM2015_Preliminary.zip
+
+zip STAR_QM2015_Preliminary.zip STAR_QM2015_Preliminary.root STAR_QM2015_Preliminary.txt print_STAR_QM2015_Preliminary.C
 
 rm -f *.so *.d include/*.so include/*.d
